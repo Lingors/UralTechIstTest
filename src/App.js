90ca262a193391346c14fd72887386f29a68dcd1
Lingors <maks.26rus1997@mail.ru>
2022-08-27 18:05:18 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import {useEffect, useState} from "react";
 
 function App() {
-    const [ip, setIP] = useState('');
-    const [city, setCity] = useState('')
-    const [streets, setStreets] = useState([])
+    const [ip, setIP] = useState('') // инициализируем переменную "ip"
+    const [city, setCity] = useState('') // инициализируем переменную "city"
+    const [streets, setStreets] = useState([]) // инициализируем переменную "streets"
 
-    const token = "3147d8ae4aacd8df71062175be76c4e543129ba0";
+    const token = "3147d8ae4aacd8df71062175be76c4e543129ba0" // токен для доступа к api dadata
 
-    const getStreet = () => {
-        const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
-        const street = document.getElementById('street-choice').value
-        if (street !== '') {
-            const query = city.toLowerCase() + ' ' + street.toLowerCase()
+    const getStreets = () => { // функция для получения списка улиц
+        const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address" // юрл к api dadata
+        const street = document.getElementById('street-choice').value // получаем введенные данные в поле улиц
+        if (street !== '') { // если не пусто то начинаем получать список улиц
+            const query = city.toLowerCase() + ' ' + street.toLowerCase() // создадим строку для api
 
-            const options = {
+            const options = { // опции которые передаются к api dadata, включают метод, хедерс и тело запроса
                 method: "POST",
                 mode: "cors",
                 headers: {
@@ -23,27 +23,28 @@ function App() {
                     "Authorization": "Token " + token
                 },
                 body: JSON.stringify({query: query})
-            };
+            }
 
-            fetch(url, options)
-                .then(response => response.json())
-                .then(result => setStreets(result.suggestions))
-                .catch(error => console.log("error", error));
+            fetch(url, options)// делаем запрос к api
+                .then(response => response.json()) // преобразуем ответ в json
+                .then(result => setStreets(result.suggestions)) // инициализируем переменную "streets" полученный список
+                .catch(error => console.log("error", error)) // если ошибка выводим ее в консоль
         } else {
-            setStreets([])
+            setStreets([]) // если поле input пустое, то инициализируем переменную "streets" пустым массивом
         }
     }
 
-    function noDigits(event) {
-        if ("1234567890".indexOf(event.key) !== -1)
-            event.preventDefault();
-        if (event.key === 'Enter')
-            getStreet()
+    function noDigits(event) { // функция для проверки вводимых заначений в поле input, а также проверка не нажатали кнопка Enter
+        if (event.key.match(/[^a-zA-Zа-яА-Я]/g)) // если введены любые символы кроме букв, поле инпут не будет изменяться
+            event.preventDefault()
+        if (event.key === 'Enter') // если нажата кнопка Enter - запускаем функцию для получения списка улиц
+            getStreets()
     }
 
-    useEffect(() => {
-        const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?ip=";
-        const options = {
+    useEffect(() => { // react hook срабатывающей сразу после рендера компонента
+        const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?ip=" // юрл к api dadata
+
+        const options = { // опции которые передаются к api dadata, включают метод и хедерс
             method: "GET",
             mode: "cors",
             headers: {
@@ -51,16 +52,16 @@ function App() {
                 "Accept": "application/json",
                 "Authorization": "Token " + token
             }
-        };
+        }
 
-        fetch('https://geolocation-db.com/json/')
-            .then(response => response.json())
+        fetch('https://geolocation-db.com/json/') // делаем запрос для получения ip
+            .then(response => response.json()) // преобразуем ответ в json
             .then(result => {
-                setIP(result.IPv4)
-                fetch(url + result.IPv4, options)
-                    .then(response => response.json())
-                    .then(result => setCity(result.location.data.city))
-                    .catch(error => console.log("error", error));
+                setIP(result.IPv4) // инициализируем переменную "ip" полученным ip
+                fetch(url + result.IPv4, options) // делаем запрос к api dadata с полученным ip
+                    .then(response => response.json()) // преобразуем ответ в json
+                    .then(result => setCity(result.location.data.city)) // инициализируем переменную "city" полученным городом
+                    .catch(error => console.log("error", error)); // если ошибка, выводим ее в консоль
             })
 
 
@@ -69,14 +70,16 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>IP: {ip}</h1>
-                <h1>Город: {city}</h1>
+                <h1>IP: {ip}</h1> {/*  поле для ip  */}
+                <h1>Город: {city}</h1> {/*  поле для города  */}
                 <div>
                     <label htmlFor="street-choice">Введите улицу: </label>
-                    <input placeholder="Введите улицу" id="street-choice" name="street-choice" autoComplete={'off'} onKeyPress={noDigits}/>
+                    <input placeholder="Введите улицу" id="street-choice" name="street-choice" autoComplete={'off'}
+                           onKeyPress={noDigits}/> {/*  поле для ввода улицы  */}
                 </div>
                 <select>
-                    {streets.map((elem, index) => <option key={index}>{elem.unrestricted_value}</option>)}
+                    {streets.map((elem, index) => <option
+                        key={index}>{elem.unrestricted_value}</option>)} {/*  пробегаем по массиву улиц и рендерим их внутри селекта  */}
                 </select>
             </header>
         </div>
